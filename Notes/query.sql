d8bec53e268b3cc1e8c36a882cef7323d22b7433
Hiral <hiral.mashru239@gmail.com>
2021-01-25 19:59:04 +0530

    sql

---
+++ Notes/query.sql
@@ -0,0 +1,19 @@
+SELECT * FROM practice.category;
+
+SELECT p.title as 'Product Title', c.title as 'Category Title' from practice.product p
+left join practice.product_category pc on pc.productId = p.id  
+left join practice.category c on c.id = pc.categoryId;
+
+SELECT p.title as 'Product Title', pc.categoryId as 'Category Title' from practice.product p
+right join practice.product_category pc on pc.productId = p.id;
+
+SELECT * from practice.product
+WHERE product.id NOT IN (SELECT productId from practice.product_category);
+
+select product_tag.productId, tag.title from product_tag LEFT JOIN tag ON product_tag.tagId = tag.id;
+
+select product_tag.productId, tag.title from product_tag RIGHT JOIN tag ON product_tag.tagId = tag.id;
+
+select firstName, middleName, lastName from cart where country NOT IN ('Pakistan');
+
+select firstName, middleName, lastName from cart where country IN ('Pakistan');
