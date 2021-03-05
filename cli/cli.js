e0f7d16b3af96b18ae4f7a55ccf6ba24825649af
Hiral <hiral.mashru239@gmail.com>
2021-03-05 19:21:40 +0530

    structure

--- cli/cli.js
+++ cli/cli.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 
-console.log( "Hello!" );
-
 const fs = require('fs');
 const path = require('path')
 const chalk = require('chalk')
-
+const Confirm = require('prompt-confirm')
 const [,,...args] = process.argv // to parse command line arguments
 const[type,...modulle] = args
 const rootDir = process.cwd()
+const download = require('download-git-repo')
+var flag = false
 
-if(type==='practice') {
+if(type==='create-folder') {
     
     if(modulle){
         if (!fs.existsSync(String(modulle))) {
@@ -28,76 +28,27 @@ if(type==='practice') {
     }
 
 } else if(type === 'init') {
-
-    fs.mkdir(path.join(rootDir, 'api'),{ recursive: true }, (err) => { 
-        if (err) { 
-            return console.error(err); 
-        } 
-        console.log(chalk.green('Directory \'api\' created successfully!')); 
-    });
-    fs.mkdir(path.join(rootDir, 'config'), { recursive: true },(err) => { 
-        if (err) { 
-            return console.error(err); 
-        } 
-        console.log(chalk.green('Directory \'config\' created successfully!')); 
-    });
-    fs.mkdir(path.join(rootDir, 'core'),{ recursive: true }, (err) => { 
-        if (err) { 
-            return console.error(err); 
-        } 
-        console.log(chalk.green('Directory \'core\' created successfully!')); 
-    });
-    fs.mkdir(path.join(rootDir, 'db'),{ recursive: true }, (err) => { 
-        if (err) { 
-            return console.error(err); 
-        } 
-        console.log(chalk.green('Directory \'db\' created successfully!')); 
-    });
-    fs.mkdir(path.join(rootDir, 'globalMiddleware'),{ recursive: true }, (err) => { 
-        if (err) { 
-            return console.error(err); 
-        } 
-        console.log(chalk.green('Directory \'globalMiddleware\' created successfully!')); 
-    });
-    fs.mkdir(path.join(rootDir, 'src'), (err) => { 
-        if (err) { 
-            return console.error(err); 
-        } 
-        console.log(chalk.green('Directory \'src\' created successfully!')); 
-    });
-    if(!fs.existsSync(rootDir+'/.env')) {
-        fs.writeFile(path.join(rootDir, '.env'),'', function(err, result) {
-            if(err) console.log('error', err);
+    if(fs.existsSync(rootDir+'/.env')){
+        new Confirm('You have already done initialization. Do you want to do init?')
+        .run()
+        .then(function(answer){
+            if(answer){
+                download('hiral-mashru/boilerplate-structure', rootDir, function (err) {
+                    flag = true
+                    console.log(err ? 'Error' : 'Success '+flag)
+                    if(!err){
+                        createStructure()
+                    }
+                })
+            }
         })
-    }
-    if(!fs.existsSync(rootDir+'/core/connection.js')) {
-        fs.writeFile(path.join(rootDir, 'core', 'connection.js'),'', function(err, result) {
-            if(err) console.log('error', err);
-        })
-    }
-    if(!fs.existsSync(rootDir+'/core/migration.js')) {   
-        fs.writeFile(path.join(rootDir, 'core', 'migration.js'),'', function(err, result) {
-            if(err) console.log('error', err);
-        })
-    }
-    if(!fs.existsSync(rootDir+'/core/routes.js')) {
-        fs.writeFile(path.join(rootDir, 'core', 'routes.js'),'', function(err, result) {
-            if(err) console.log('error', err);
-        })
-    }
-    if(!fs.existsSync(rootDir+'/core/models.js')) {
-        fs.writeFile(path.join(rootDir, 'core', 'models.js'),'', function(err, result) {
-            if(err) console.log('error', err);
-        })
-    }
-    if(!fs.existsSync(rootDir+'/core/services.js')) {
-        fs.writeFile(path.join(rootDir, 'core', 'services.js'),'', function(err, result) {
-            if(err) console.log('error', err);
-        })
-    }
-    if(!fs.existsSync(rootDir+'/src/app.js')) {
-        fs.writeFile(path.join(rootDir, 'src', 'app.js'),'', function(err, result) {
-            if(err) console.log('error', err);
+    } else {
+        download('hiral-mashru/boilerplate-structure', rootDir, function (err) {
+            flag = true
+            console.log(err ? 'Error' : 'Success '+flag)
+            if(!err){
+                createStructure()
+            }
         })
     }
 
@@ -133,8 +84,107 @@ if(type==='practice') {
         }
     }
 
-} else {
+} 
+// else if(type === 'github'){
+//     download('hiral-mashru/boilerplate-structure', '', function (err) {
+//         console.log(err ? 'Error' : 'Success')
+//     })
+// }
+else {
     console.log(chalk.black.bgYellowBright('WARNING:')+' Type is not provided')
 }
 
 
+function createStructure(){
+    fs.mkdir(path.join(rootDir, 'api'),{ recursive: true }, (err) => { 
+        if (err) { 
+            return console.error(err); 
+        } 
+        console.log(chalk.green('Directory \'api\' created successfully!')); 
+    });
+    fs.mkdir(path.join(rootDir, 'config'), { recursive: true },(err) => { 
+        if (err) { 
+            return console.error(err); 
+        } 
+        console.log(chalk.green('Directory \'config\' created successfully!')); 
+    });
+    fs.mkdir(path.join(rootDir, 'core'),{ recursive: true }, (err) => { 
+        if (err) { 
+            return console.error(err); 
+        } 
+        console.log(chalk.green('Directory \'core\' created successfully!')); 
+    });
+    fs.mkdir(path.join(rootDir, 'db'),{ recursive: true }, (err) => { 
+        if (err) { 
+            return console.error(err); 
+        } 
+        console.log(chalk.green('Directory \'db\' created successfully!')); 
+    });
+    fs.mkdir(path.join(rootDir, 'db','models'),{ recursive: true }, (err) => { 
+        if (err) { 
+            return console.error(err); 
+        } 
+        console.log(chalk.green('Directory \'db/models\' created successfully!')); 
+    });
+    fs.mkdir(path.join(rootDir, 'db','migrations'),{ recursive: true }, (err) => { 
+        if (err) { 
+            return console.error(err); 
+        } 
+        console.log(chalk.green('Directory \'db/migrations\' created successfully!')); 
+    });
+    fs.mkdir(path.join(rootDir, 'db','seeders'),{ recursive: true }, (err) => { 
+        if (err) { 
+            return console.error(err); 
+        } 
+        console.log(chalk.green('Directory \'db/seeders\' created successfully!')); 
+    });
+    fs.mkdir(path.join(rootDir, 'middlewares'),{ recursive: true }, (err) => { 
+        if (err) { 
+            return console.error(err); 
+        } 
+        console.log(chalk.green('Directory \'middlewares\' created successfully!')); 
+    });
+    fs.mkdir(path.join(rootDir, 'src'),{ recursive: true }, (err) => { 
+        if (err) { 
+            return console.error(err); 
+        } 
+        console.log(chalk.green('Directory \'src\' created successfully!')); 
+    });
+    if(!fs.existsSync(rootDir+'/.env')) {
+        fs.writeFile(path.join(rootDir, '.env'),'', function(err, result) {
+            if(err) console.log('error', err);
+        })
+    }
+    if(!fs.existsSync(rootDir+'/core/connection.js')) {
+        fs.writeFile(path.join(rootDir, 'core', 'connection.js'),'', function(err, result) {
+            if(err) console.log('error', err);
+        })
+    }
+    if(!fs.existsSync(rootDir+'/core/migrations.js')) {   
+        fs.writeFile(path.join(rootDir, 'core', 'migrations.js'),'', function(err, result) {
+            if(err) console.log('error', err);
+        })
+    }
+    if(!fs.existsSync(rootDir+'/core/routes.js')) {
+        fs.writeFile(path.join(rootDir, 'core', 'routes.js'),'', function(err, result) {
+            if(err) console.log('error', err);
+        })
+    }
+    if(!fs.existsSync(rootDir+'/core/models.js')) {
+        fs.writeFile(path.join(rootDir, 'core', 'models.js'),'', function(err, result) {
+            if(err) console.log('error', err);
+        })
+    }
+    if(!fs.existsSync(rootDir+'/core/services.js')) {
+        fs.writeFile(path.join(rootDir, 'core', 'services.js'),'', function(err, result) {
+            if(err) console.log('error', err);
+        })
+    }
+    if(!fs.existsSync(rootDir+'/src/app.js')) {
+        fs.writeFile(path.join(rootDir, 'src', 'app.js'),'', function(err, result) {
+            if(err) {console.log('error', err);}
+            console.log('exists '+fs.existsSync(rootDir+'/src/app.js'))
+        })
+    }
+    console.log(chalk.green('Boilerplate structure is created...'))
+}
---
+++ folder/functions/funcFile.js
@@ -0,0 +1,7 @@
+function func1 (){
+    console.log("Function 1")
+}
+
+setup.functions = {func1: func1()}
+
+setup.func1
