16da25d9f1dadc48e8e3551622db789a3440fe86
Hiral <hiral.mashru239@gmail.com>
2021-04-06 02:26:37 +0530

    setup

--- apis/config/config.js
+++ apis/config/config.js
@@ -9,11 +9,14 @@ async function morgn(app){
     morgan.token('req-body', function(req,res){
       return JSON.stringify(req.body)
     })  
-    if(config.length===1 && config[0] === "req-headers"){
+    // if(config.length===1 && config[0] === "req-headers"){
+    if(config["req-headers"]){
       app.use(morgan(':remote-addr - ":method :url HTTP/:http-version" :status :req-headers :res[content-length] [:date[clf]]'))
-    } else if(config.length===1 && config[0] === "req-body"){
+    // } else if(config.length===1 && config[0] === "req-body"){
+    } else if(config["req-body"]){
       app.use(morgan(':remote-addr - ":method :url HTTP/:http-version" :status :req-body :res[content-length] [:date[clf]]'))
-    } else if(config.length===2 && config.includes("req-body") && config.includes("req-headers")){
+    // } else if(config.length===2 && config.includes("req-body") && config.includes("req-headers")){
+    } else if(config["req-body"] && config["req-headers"]){
       app.use(morgan(':remote-addr - ":method :url HTTP/:http-version" :status :req-headers :req-body :res[content-length] [:date[clf]]'))
     } else {
       app.use(morgan(':remote-addr - ":method :url HTTP/:http-version" :status :res[content-length] [:date[clf]]'))
