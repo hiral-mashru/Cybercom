41c32abf70723e911e2a588a772f50534974cc96
Hiral <hiral.mashru239@gmail.com>
2021-04-30 22:47:01 +0530

    pagination

--- demo/src/app.js
+++ demo/src/app.js
@@ -58,6 +58,50 @@ app.get("/auth/google/authorised",passport.authenticate('google'),(req,res,next)
   res.send("Authenticated")
 })
 
+const users = [
+  { id: 1, name: 'User 1'},
+  { id: 2, name: 'User 2'},
+  { id: 3, name: 'User 3'},
+  { id: 4, name: 'User 4'},
+  { id: 5, name: 'User 5'},
+  { id: 6, name: 'User 6'},
+  { id: 7, name: 'User 7'},
+  { id: 8, name: 'User 8'},
+  { id: 9, name: 'User 9'},
+  { id: 10, name: 'User 10'},
+  { id: 11, name: 'User 11'},
+]
+
+app.get('/users',paginatedResults(users),(req,res)=>{
+  res.json(res.paginatedResults)
+})
+
+function paginatedResults(model){
+  return (req,res,next)=>{
+    const page = parseInt(req.query.page,10) || 1
+    const limit = parseInt(req.query.limit,10) || 10
+    const offset = page * limit - limit
+    const startIndex = (page-1)*limit
+    const endIndex = page*limit
+    const results = {}
+    if(endIndex<model.length){
+      results.next = {
+        page: page+1,
+        limit: limit
+      }
+    }
+    if(startIndex>0){
+      results.previous = {
+        page: page-1,
+        limit: limit
+      }
+    }
+    results.results = model.slice(startIndex,endIndex)
+    res.paginatedResults = results
+    next()
+  }
+}
+
 ///////////////////////////////////////////////////////
       try{
           for(let key in routes.public){    
